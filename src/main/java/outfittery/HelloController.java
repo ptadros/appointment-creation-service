ac3876a72e6ee363b65863c9e055d911b2ace490
Peter Tadros <peter.aboalim@gmail.com>
2019-02-09 15:59:21 +0100

    refactor: change package names and added test for CustomerRepository

--- src/main/java/outfittery/HelloController.java
+++ src/main/java/outfittery/HelloController.java
@@ -1,4 +1,4 @@
-package hello;
+package outfittery;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/outfittery/HelloControllerIT.java
+++ src/test/java/outfittery/HelloControllerIT.java
@@ -1,4 +1,4 @@
-package hello;
+package outfittery;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
