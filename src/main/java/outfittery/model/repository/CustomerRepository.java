248af2f5694ec9c1578ef8c2c83d08649a1d31f7
Peter Tadros <peter.aboalim@gmail.com>
2019-02-10 00:27:21 +0100

    refactor: remove unused code

--- src/main/java/outfittery/model/repository/CustomerRepository.java
+++ src/main/java/outfittery/model/repository/CustomerRepository.java
@@ -1,14 +1,9 @@
 package outfittery.model.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import outfittery.model.entity.Customer;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-
-    List<Customer> findByEmail(String email);
-    
+public interface CustomerRepository extends CrudRepository<Customer, Long> {    
     Boolean existsByEmail(String email);
 }
